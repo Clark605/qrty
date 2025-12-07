0106bbe31b4594f04b3ab6792d5d5fbe41533662
ZBOOK\Clark <clarkremon12@gmail.com>
2025-12-07 22:06:40 +0200

    - implemented splash screen

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,28 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:qrty/core/theme/app_colors.dart';
 
-ThemeData appTheme = ThemeData(
-  primaryColor: AppColors.primary,
-  scaffoldBackgroundColor: AppColors.background,
+class AppTheme {
+  static ThemeData appTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.background,
 
-  textTheme: const TextTheme(
-    headlineMedium: TextStyle(
-      color: AppColors.titleGrey,
-      fontSize: 27,
-      fontWeight: FontWeight.normal,
-      fontFamily: 'Itim',
+    textTheme: const TextTheme(
+      headlineMedium: TextStyle(
+        color: AppColors.titleGrey,
+        fontSize: 27,
+        fontWeight: FontWeight.normal,
+        fontFamily: 'Itim',
+      ),
+      titleMedium: TextStyle(
+        color: AppColors.titleGrey,
+        fontSize: 16,
+        fontWeight: FontWeight.normal,
+        fontFamily: 'Itim',
+      ),
+      bodyMedium: TextStyle(
+        color: AppColors.bodyGrey,
+        fontSize: 14,
+        fontWeight: FontWeight.normal,
+        fontFamily: 'Itim',
+      ),
     ),
-    titleMedium: TextStyle(
-      color: AppColors.titleGrey,
-      fontSize: 16,
-      fontWeight: FontWeight.normal,
-      fontFamily: 'Itim',
-    ),
-    bodyMedium: TextStyle(
-      color: AppColors.bodyGrey,
-      fontSize: 14,
-      fontWeight: FontWeight.normal,
-      fontFamily: 'Itim',
-    ),
-  ),
-);
+  );
+}
