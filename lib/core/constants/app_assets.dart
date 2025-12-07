7ae818b8945144e398e8240cf5ffe00ed7ee4cea
ZBOOK\Clark <clarkremon12@gmail.com>
2025-12-07 21:30:49 +0200

    Add app assets, colors, and theme configuration
    - Created AppAssets class to manage image and icon paths.
    - Introduced AppColors class for consistent color management.
    - Defined appTheme with primary color, background color, and text styles.

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,31 @@
+class AppAssets {
+  //images
+  static const String logo = 'assets/images/logo.png';
+  static const String splash = 'assets/images/background.png';
+
+  //icons
+  static const String business = 'assets/icons/business.svg';
+  static const String contact = 'assets/icons/contact.svg';
+  static const String copy = 'assets/icons/copy.svg';
+  static const String email = 'assets/icons/email.svg';
+  static const String event = 'assets/icons/event.svg';
+  static const String flash = 'assets/icons/flash.svg';
+  static const String flipCamera = 'assets/icons/flip_camera.svg';
+  static const String history = 'assets/icons/history.svg';
+  static const String images = 'assets/icons/images.svg';
+  static const String instagram = 'assets/icons/instagram.svg';
+  static const String location = 'assets/icons/location.svg';
+  static const String notification = 'assets/icons/notification.svg';
+  static const String privacy = 'assets/icons/privacy.svg';
+  static const String qr = 'assets/icons/qr.svg';
+  static const String qrAppBar = 'assets/icons/qr_app_bar.svg';
+  static const String rate = 'assets/icons/rate.svg';
+  static const String save = 'assets/icons/save.svg';
+  static const String share = 'assets/icons/share.svg';
+  static const String telephone = 'assets/icons/telephone.svg';
+  static const String text = 'assets/icons/text.svg';
+  static const String vibrate = 'assets/icons/vibrate.svg';
+  static const String website = 'assets/icons/website.svg';
+  static const String whatsapp = 'assets/icons/whatsapp.svg';
+  static const String wifi = 'assets/icons/wifi.svg';
+}
---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primary = Color(0xFFFDB623);
+  static const Color secondary = Color(0xFF333333);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color background = Color(0xDD333333);
+
+  static const Color titleGrey = Color(0xD9D9D9D9);
+  static const Color bodyGrey = Color(0xA4A4A4A4);
+}
