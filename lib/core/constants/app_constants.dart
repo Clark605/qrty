53dd438442d6fb9e1b3ab45f7d8a8c7d1668d01c
ZBOOK\Clark <clarkremon12@gmail.com>
2025-12-08 10:09:54 +0200

    Add localization support with Arabic and English translations, update dependencies

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+abstract class AppConstants {
+  static const String appName = 'QRty';
+
+  static const String translationsPath = 'assets/translations';
+  static const List<Locale> supportedLocales = [Locale('en'), Locale('ar')];
+}
---
+++ lib/l10n/locale_keys.g.dart
@@ -0,0 +1,40 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: constant_identifier_names
+
+abstract class  LocaleKeys {
+  static const get_started = 'get_started';
+  static const get_started_subtitle = 'get_started_subtitle';
+  static const generate = 'generate';
+  static const history = 'history';
+  static const generate_qr = 'generate_qr';
+  static const text = 'text';
+  static const website = 'website';
+  static const wifi = 'wifi';
+  static const event = 'event';
+  static const contact = 'contact';
+  static const business = 'business';
+  static const location = 'location';
+  static const whatsapp = 'whatsapp';
+  static const email = 'email';
+  static const twitter = 'twitter';
+  static const instagram = 'instagram';
+  static const telephone = 'telephone';
+  static const settings = 'settings';
+  static const vibrate = 'vibrate';
+  static const vibration_when_scan_is_done = 'vibration_when_scan_is_done';
+  static const beep = 'beep';
+  static const beep_when_scan_is_done = 'beep_when_scan_is_done';
+  static const support = 'support';
+  static const rate_us = 'rate_us';
+  static const your_best_reward_to_us = 'your_best_reward_to_us';
+  static const privacy_policy = 'privacy_policy';
+  static const follow_our_policies_that_benefits_you = 'follow_our_policies_that_benefits_you';
+  static const share = 'share';
+  static const share_app_with_others = 'share_app_with_others';
+  static const copy = 'copy';
+  static const copy_to_clipboard = 'copy_to_clipboard';
+  static const save = 'save';
+  static const result = 'result';
+
+}
