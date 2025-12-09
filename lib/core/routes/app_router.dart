9d80e271ebe6347ffce15fa265a70228f50f7c01
ZBOOK\Clark <clarkremon12@gmail.com>
2025-12-09 11:28:54 +0200

    feat(settings): integrate settings with router
    - Add settings route case in AppRouter
    - Wrap SettingsScreen with BlocProvider for SettingsCubit
    - Use AnimationRoute for consistent navigation animation
    - Import SettingsCubit and SettingsScreen

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -9,6 +9,8 @@ import 'package:qrty/feature/qr_view/cubit/qr_view_cubit.dart';
 import 'package:qrty/feature/qr_view/view/qr_view_screen.dart';
 import 'package:qrty/feature/scan_qr/cubit/scan_qr_cubit.dart';
 import 'package:qrty/feature/scan_qr/view/scan_qr_screen.dart';
+import 'package:qrty/feature/settings/cubit/settings_cubit.dart';
+import 'package:qrty/feature/settings/view/settings_screen.dart';
 import 'package:qrty/feature/splash/splash_screen.dart';
 
 class AppRouter {
@@ -37,6 +39,13 @@ class AppRouter {
               source: arg['source'] as String,
               type: arg['type'] as QRCodeType,
             ),
+          ),
+        );
+      case Routes.settings:
+        return AnimationRoute(
+          page: BlocProvider(
+            create: (context) => SettingsCubit(),
+            child: const SettingsScreen(),
           ),
         );
       default:
